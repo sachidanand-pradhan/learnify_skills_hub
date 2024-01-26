da88fc354783755ecb3538590de604ae3a7c49be
Sachidanand Pradhan <53556482+sachidanand-pradhan@users.noreply.github.com>
2024-01-26 22:09:33 +0530

    commit

--- app/(dashboard)/_components/Logo.tsx
+++ app/(dashboard)/_components/Logo.tsx
@@ -4,7 +4,7 @@ const Logo = () => {
   return (
     <div className="flex">
       <Image height={50} width={50} alt="logo" src="/logo.svg" />
-      <p className="text-[#790977] font-bold">LearniFyHub</p>
+      <p className="text-[#0369a1] font-bold">LearniFyHub</p>
     </div>
   );
 };
